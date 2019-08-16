12966b85adaf3329d1f38e86db1e67d8d4bdfa53
Bruno <bellmann.brun@gmail.com>
2019-08-16 13:28:57 +0200

    initial

---
+++ jexam.py
@@ -0,0 +1,49 @@
+# coding: utf8
+
+import urllib.request
+import sched
+import time
+import requests
+import re
+
+lookfor = ["Kodierung", "Datenbanken", "Rechnernetze", "Rechnerarchitektur", "CompilerBau", "Technische Grundlagen", "Theoretische Informatik und Logik"]
+
+def telegram_bot_sendtext(user, msg):
+    
+    bot_token = '813069260:AAES7a8FLrvmxK47LitZN7Y3DKJRBU-2y_0'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + user + '&parse_mode=Markdown&text=' + msg
+
+    response = requests.get(send_text)
+    return response.json()
+
+def checkForUpdate():
+    url = 'http://jexamgroup.blogspot.com'
+    testsonline = []
+    req = urllib.request.Request(url)
+    resp = urllib.request.urlopen(req)
+    respData = resp.read()
+    heading = re.findall(r'<a(.*?)/a>', str(respData))
+    for h in heading:
+        if "Sommersemester 2019" in h:
+            paragraphs = re.findall(r'<li>(.*?)</li>',str(respData))
+            for eachP in paragraphs:
+                if "Es wird" in eachP:
+                    break
+
+                eachP = eachP.replace('&nbsp;', '')
+                eachP = eachP.replace('\\xc3\\xbc', 'ü')
+                eachP = eachP.replace('\\xc3\\xa4', 'ä')
+                eachP = eachP.replace('\\xc3\\xb6', 'ö')
+                testsonline.append(eachP)
+
+    bot_chatIDs = ['-1001345463367']
+    for user in bot_chatIDs:
+        for item in lookfor:
+            for itemx in testsonline:
+                if item in itemx:
+                    telegram_bot_sendtext(user ,itemx + ' ist online.')
+                    lookfor.remove(item)
+    time.sleep(600)
+
+while True:
+    checkForUpdate()
